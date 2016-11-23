20df7c4482f80a97aac8f123dff921af31ad0c45
Alexey Mozhenin <amozhenin@issart.com>
2016-11-24 03:44:56 +0600

    GameTarget in process. Still needs a Lane stuff. Written initial version of action generation

--- src/main/java/GameTarget.java
+++ src/main/java/GameTarget.java
@@ -6,32 +6,44 @@ import model.*;
 public class GameTarget {
 
     private final TargetType type;
+    private final CircularUnit target;
 
     public GameTarget(Building building) {
         type = TargetType.BUILDING;
+        target = building;
     }
 
     public GameTarget(Wizard wizard) {
         type = TargetType.WIZARD;
+        target = wizard;
     }
 
     public GameTarget(Minion minion) {
         type = TargetType.MINION;
+        target = minion;
     }
 
     public GameTarget(Tree tree) {
         type = TargetType.TREE;
+        target = tree;
     }
 
     public GameTarget(Bonus bonus) {
         type = TargetType.BONUS;
+        target = bonus;
     }
 
     public GameTarget(Lane lane) {
         type = TargetType.LANE;
+        //TODO work this out
+        target = null;
     }
 
     public TargetType getTargetType() {
         return type;
+    }
+
+    public CircularUnit getTarget() {
+        return target;
     }
 }
--- src/main/java/MyStrategy.java
+++ src/main/java/MyStrategy.java
@@ -17,11 +17,6 @@ public final class MyStrategy implements Strategy {
 
     @Override
     public void move(Wizard self, World world, Game game, Move move) {
-//        move.setSpeed(game.getWizardForwardSpeed());
-////        move.setStrafeSpeed(game.getWizardStrafeSpeed());
-////        move.setTurn(game.getWizardMaxTurnAngle());
-//        move.setAction(ActionType.MAGIC_MISSILE);
-
         List<GameAction> gameActions = generateActions(self, world, game);
         List<EstimatedGameAction> estimatedActions = estimate(gameActions);
         List<EstimatedGameAction> bestActions = selectActionsToDo(estimatedActions);
@@ -29,12 +24,40 @@ public final class MyStrategy implements Strategy {
     }
 
     private List<GameAction> generateActions(Wizard self, World world, Game game) {
-        //TODO implement
         List<GameAction> actions = new ArrayList<>();
-        actions.add(new GameAction(Action.ADVANCE, null));
+        actions.add(new GameAction(Action.ADVANCE, new GameTarget(new Lane(LaneType.TOP))));
+        actions.add(new GameAction(Action.ADVANCE, new GameTarget(new Lane(LaneType.MIDDLE))));
+        actions.add(new GameAction(Action.ADVANCE, new GameTarget(new Lane(LaneType.BOTTOM))));
         //world.getBuildings()
-        actions.add(new GameAction(Action.ATTACK, null));
+        for (Building building : world.getBuildings()) {
+            if (isFoe(self.getFaction(), building)) {
+                actions.add(new GameAction(Action.ATTACK, new GameTarget(building)));
+            }
+        }
+        for (Wizard wizard : world.getWizards()) {
+            if (wizard.isMe()) {
+                continue;
+            }
+            if (isFoe(self.getFaction(), wizard)) {
+                actions.add(new GameAction(Action.ATTACK, new GameTarget(wizard)));
+            }
+        }
+        for (Minion minion: world.getMinions()) {
+            if (isFoe(self.getFaction(), minion)) {
+                actions.add(new GameAction(Action.ATTACK, new GameTarget(minion)));
+            }
+        }
         return actions;
+    }
+
+    private boolean isFoe(Faction faction, CircularUnit unit) {
+        if (unit.getFaction() == faction) {
+            return false;
+        }
+        if (unit.getFaction() == Faction.OTHER) {
+            return false;
+        }
+        return true;
     }
 
     private List<EstimatedGameAction> estimate(List<GameAction> actions) {
